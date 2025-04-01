905f174280507a88b2189f43fa95162e83398876
Fabiano <fabipiroli@live.com>
2025-03-31 21:48:43 -0300

    commit 2

--- Aula03/Aula03/obj/Debug/net9.0/Aula03.AssemblyInfo.cs
+++ Aula03/Aula03/obj/Debug/net9.0/Aula03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd0ec7e3e36a3cc89b5f8866b379cb1fd60ab22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27dfa72bca8f4e534345dbe116503c0fe51bed79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
