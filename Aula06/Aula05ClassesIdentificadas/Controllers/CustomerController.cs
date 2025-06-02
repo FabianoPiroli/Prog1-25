ec88b6f9b02c86f05bcc0306f6b0325369643760
Fabiano <fabipiroli@live.com>
2025-06-02 20:36:14 -0300

    teste

--- Aula06/Aula05ClassesIdentificadas/Controllers/CustomerController.cs
+++ Aula06/Aula05ClassesIdentificadas/Controllers/CustomerController.cs
@@ -10,7 +10,9 @@ namespace Aula05ClassesIdentificadas.Controllers
 
         private CustomerRepository _customerRepository;
 
-        public CustomerController()
+        public CustomerController(
+            IWebHostEnvironment environment
+        )
         {
             _customerRepository = new CustomerRepository();
         }
@@ -26,6 +28,40 @@ namespace Aula05ClassesIdentificadas.Controllers
         [HttpGet]
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ExportDelimitatedFile()
+        {
+            string fileContent = string.Empty;
+            foreach (Customer c in CustomerData.Customers)
+            {
+                fileContent +=
+                                @$"{c.Id};
+                                  {c.Name};
+                                  {c.HomeAddress?.Id};
+                                  {c.HomeAddress?.City};
+                                  {c.HomeAddress?.State};
+                                  {c.HomeAddress?.Country};
+                                  {c.HomeAddress?.StreetLine1};
+                                  {c.HomeAddress?.StreetLine2};
+                                  {c.HomeAddress?.PostalCode};
+                                  {c.HomeAddress?.AddressType}
+                                   \n
+                               ";
+            }
+
+            var filepath = Path.Combine(enviroment.WebRootPath, "TextFiles", "Delimitado.txt");
+
+            if(!System.IO.File.Exists(filepath))
+            {
+                using (StreamWriter sw = System.IO.File.CreateText(filepath))
+                {
+                    sw.WriteLine(fileContent);
+                }
+            }
+
             return View();
         }
 
